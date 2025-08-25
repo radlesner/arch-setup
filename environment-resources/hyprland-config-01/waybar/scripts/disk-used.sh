cd5607eb0323ca5507ff9ce7430eae17716288b2
radlesner <radeklesner2000@gmail.com>
2025-08-25 16:44:34 +0200

    Update Hyprland config 01.

--- environment-resources/hyprland-config-01/waybar/scripts/disk-used.sh
+++ environment-resources/hyprland-config-01/waybar/scripts/disk-used.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mount_path="/home"
+mount_path="$1"
 
 read total used free <<< $(df -P "$mount_path" | awk 'NR==2 {print $2, $3, $4}')
 used_percent=$(awk "BEGIN {printf \"%.0f\", ($used / $total) * 100}")
@@ -9,6 +9,6 @@ total_gb=$(awk "BEGIN {printf \"%.1f\", $total / 1024 / 1024}")
 used_gb=$(awk "BEGIN {printf \"%.1f\", $used / 1024 / 1024}")
 free_gb=$(awk "BEGIN {printf \"%.1f\", $free / 1024 / 1024}")
 
-echo "󱂵 ${used_percent}%"
+echo " ${used_percent}%"
 
-echo "/home: ${used_gb} GiB / ${total_gb} GiB (free: ${free_gb} GiB)"
+echo "$mount_path: ${used_gb} GiB / ${total_gb} GiB (free: ${free_gb} GiB)"
