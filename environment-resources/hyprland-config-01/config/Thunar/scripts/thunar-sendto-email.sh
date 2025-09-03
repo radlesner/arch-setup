22f8baec5b0feafb6b1f2706fe988bb50a8f8769
radlesner <radeklesner2000@gmail.com>
2025-09-03 20:12:15 +0200

    Added merge audio-video custom action.

--- environment-resources/hyprland-config-01/config/Thunar/scripts/thunar-sendto-email.sh
+++ environment-resources/hyprland-config-01/config/Thunar/scripts/thunar-sendto-email.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Buduje listę --attach dla wszystkich plików przekazanych jako argumenty
 args=""
 for f in "$@"; do
     args+=" --attach \"$f\""
 done
 
-# Uruchamia xdg-email z gotową listą załączników
 eval xdg-email $args
