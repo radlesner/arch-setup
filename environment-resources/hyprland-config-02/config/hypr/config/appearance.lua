a503004b32a0b482f05d6b30f769fbbd2efb9880
radlesner <radeklesner2000@gmail.com>
2026-08-08 21:01:24 +0200

    feat(hyprland config 01 & 02): - replace legacy hyprland.conf with Lua configuration - split settings into dedicated modules - preserve existing keybindings and window rules - migrate monitors, workspaces, input and appearance settings - keep hyprland.conf.old as a fallback for troubleshooting

--- environment-resources/hyprland-config-02/config/hypr/config/appearance.lua
+++ environment-resources/hyprland-config-02/config/hypr/config/appearance.lua
@@ -6,6 +6,7 @@ hl.config({
     general = {
         gaps_in = 5,
         gaps_out = 20,
+<<<<<<< HEAD
 
         border_size = 2,
 
@@ -15,6 +16,23 @@ hl.config({
         resize_on_border = false,
         allow_tearing = false,
 
+=======
+        border_size = 2,
+
+        col = {
+            active_border = {
+                colors = {
+                    "rgba(33ccffee)",
+                    "rgba(00ff99ee)"
+                },
+                angle = 45
+            },
+            inactive_border = "rgba(595959aa)",
+        },
+
+        resize_on_border = false,
+        allow_tearing = false,
+>>>>>>> 936e6cf (feat(hyprland config 01 & 02):)
         layout = "dwindle",
     },
 
